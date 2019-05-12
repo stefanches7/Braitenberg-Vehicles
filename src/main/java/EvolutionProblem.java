050404294b07f87e8fed0664cde0d12799cbdfad
Stefan Dvoretskii <stefan.dvorezky@gmail.com>
2019-05-12 15:04:26 +0200

    Primitive GA running

--- src/main/java/EvolutionProblem.java
+++ src/main/java/EvolutionProblem.java
@@ -1,17 +1,21 @@
+import sun.reflect.generics.tree.Tree;
+
 import java.util.*;
 
 public class EvolutionProblem {
 
-    private static char[] genes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-    private static String solution;
+    private static char[] genes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ".toCharArray();
+    private static String solution = "";
     private static int optimalFitness;
-    private static int initialPopulation = 10;
+    private static int initialPopulation = 100;
     private static TreeMap<String, Integer> population;
     private static float mutationRate = 0.05F;
-    private static float crossoverRate = 0.5F;
-    private static float rateEliteSelected = 0.05F;
+    private static float crossoverRate = 0.8F;
+    private static float rateEliteSelected = 0.8F;
     private static float rateLuckySelected = 0.01F;
-
+    private static long startTime = 0;
+    private static long endTime = 0;
+    private static final Random r = new Random();
 
 
     public static void main(String[] args) {
@@ -27,8 +31,10 @@ public class EvolutionProblem {
             if (!isGene(arg.charAt(i)))
                 System.out.println("Target solution contains symbols which are not genes of current GA!");
         }
+        startTime = System.currentTimeMillis();
         solution = arg;
         optimalFitness = solution.length(); //require to guess the whole word
+        population = new TreeMap<>();
         // start GA
         for (int i = 0; i < initialPopulation; i++) {
             String chromosome = randomGeneSet();
@@ -42,9 +48,27 @@ public class EvolutionProblem {
             mutate(population);
             crossover(population);
             population = select(population);
-            if (!solutionAchieved(population)) ep++;
+            if (solutionAchieved(population)) {
+                long elapsedTime = System.currentTimeMillis() - startTime;
+                System.out.println("Achieved optimal fitness in " + ep + " epochs! Parameters: ");
+                System.out.println("Initial population: " + initialPopulation);
+                System.out.println("Mutation rate: " + mutationRate);
+                System.out.println("Rate individuals mating: " + crossoverRate);
+                System.out.println("Elitarism: " + rateEliteSelected);
+                System.out.println("Running time (s): " + elapsedTime/1000.0);
+                System.exit(0);
+            } else ep++;
+
+
         }
         System.out.println("Couldn't reach answer in " + maxEpochs + " epochs!");
+    }
+
+    private static boolean solutionAchieved(TreeMap<String, Integer> population) {
+        for (Map.Entry<String, Integer> individual: population.entrySet()) {
+            if (individual.getValue() >= optimalFitness) return true;
+        }
+        return false;
     }
 
     private static boolean isGene(char c) {
@@ -55,25 +79,41 @@ public class EvolutionProblem {
     }
 
     private static TreeMap<String, Integer> select(TreeMap<String, Integer> population) {
+        TreeMap<String, Integer> remaining = new TreeMap<>();
         // elitarism
-        Map<String, Integer> remaining = new TreeMap<>();
-
-        return null;
+        Map<String, Integer> sortedPop = sortByValues(population);
+        Set<Map.Entry<String, Integer>> chromosomes = sortedPop.entrySet();
+        Iterator<Map.Entry<String, Integer>> it = chromosomes.iterator();
+        for (int i = 0; i < ((int) (chromosomes.size() * rateEliteSelected)); i++) {
+            Map.Entry<String, Integer> nextChr = it.next();
+            remaining.put(nextChr.getKey(), nextChr.getValue());
+        }
+        // TODO lucky ones
+        System.out.println("Individuals that survived:");
+        remaining.forEach((k, v) -> System.out.println("Chromosome: " + k + ", fitness: " + v));
+        return remaining;
     }
 
-    static <K,V extends Comparable<? super V>>
-    SortedSet<Map.Entry<K,V>> entriesSortedByValues(Map<K,V> map) {
-        SortedSet<Map.Entry<K,V>> sortedEntries = new TreeSet<Map.Entry<K,V>>(
-                new Comparator<Map.Entry<K,V>>() {
-                    @Override public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
-                        int res = e1.getValue().compareTo(e2.getValue());
-                        return res != 0 ? res : 1;
+    public static <K, V extends Comparable<V>> Map<K, V>
+    sortByValues(final Map<K, V> map) {
+        Comparator<K> valueComparator =
+                new Comparator<K>() {
+                    public int compare(K k1, K k2) {
+                        int compare =
+                                map.get(k1).compareTo(map.get(k2));
+                        if (compare == 0)
+                            return 1;
+                        else
+                            return compare;
                     }
-                }
-        );
-        sortedEntries.addAll(map.entrySet());
-        return sortedEntries;
+                };
+
+        Map<K, V> sortedByValues =
+                new TreeMap<K, V>(valueComparator.reversed());
+        sortedByValues.putAll(map);
+        return sortedByValues;
     }
+
 
     private static void crossover(TreeMap<String, Integer> population) {
         TreeMap<String, Integer> kids = new TreeMap<>();
@@ -83,6 +123,7 @@ public class EvolutionProblem {
             while (c1.contentEquals(c2))
                 c2 = selectRandomChr(population);
             String child = mixGametes(c1, c2);
+            System.out.println("Produced child " + child);
             kids.put(child, fitness(child));
         }
         population.putAll(kids);
@@ -91,48 +132,42 @@ public class EvolutionProblem {
     private static String mixGametes(String c1, String c2) {
         if (c1.length() != c2.length())
             throw new IllegalArgumentException("Crossing two not equally long chromosomes!");
-        Random r = new Random();
         int pos = r.nextInt(c1.length());
-        String[] gametes = new String[]{c1.substring(0, pos), c1.substring(pos),
-                                        c2.substring(0, pos), c2.substring(pos)};
-        String g1 = gametes[r.nextInt(gametes.length)];
-        String g2 = String.valueOf(c1);
-        while (g1.contentEquals(g2)) g2 = gametes[r.nextInt(gametes.length)];
-        return g1 + g2;
+        String[] possibleCrosses = new String[]{c1.substring(0, pos) + c2.substring(pos),
+                c2.substring(0, pos) + c1.substring(pos)};
+        return possibleCrosses[r.nextInt(possibleCrosses.length)];
     }
 
     private static String selectRandomChr(TreeMap<String, Integer> population) {
-        Random       random    = new Random();
         List<String> keys      = new ArrayList<String>(population.keySet());
-        String       randomKey = keys.get( random.nextInt(keys.size()) );
+        String       randomKey = keys.get( r.nextInt(keys.size()) );
         return randomKey;
     }
 
     private static void mutate(TreeMap<String, Integer> population) {
-        Random random = new Random();
+        Map<String, Integer> mutated = new HashMap<>();
+        List<String> original = new LinkedList<>();
         population.forEach((chr, val) -> {
-            if (random.nextInt(100) // a 100% cointoss
+            if (r.nextInt(100) // a 100% cointoss
                     < ((int) (mutationRate * 100))) {
-                int pos = random.nextInt(chr.length()); // random position
-                String mutatedChr = chr.substring(0, pos - 1) + genes[random.nextInt(genes.length)]
+                int pos = r.nextInt(chr.length() - 1) + 1; // random position, not 0
+                String mutatedChr = chr.substring(0, pos - 1) + genes[r.nextInt(genes.length)]
                         + chr.substring(pos);
                 System.out.println("Mutated " + chr + " to " + mutatedChr);
-                population.remove(chr);
-                population.put(mutatedChr, fitness(mutatedChr));
+                original.add(chr);
+                mutated.put(mutatedChr, fitness(mutatedChr));
             }
         });
+        for (String chr: original) population.remove(chr);
+        population.putAll(mutated);
     }
 
-
-
-
     private static String randomGeneSet() {
-        String out = "";
-        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < solution.length(); i++) {
-            out += genes[r.nextInt(solution.length())];
+            sb.append(genes[r.nextInt(genes.length)]);
         }
-        return out;
+        return sb.toString();
     }
 
     private static int fitness(String chromosome) {
