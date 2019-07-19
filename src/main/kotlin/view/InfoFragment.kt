7f0b57bc95051e6848db065ba0db8d96bdd125db
Stefan Dvoretskii <stefan.dvorezky@gmail.com>
2019-07-19 19:17:19 +0200

    Add simple histogram of vehicle speeds to simulation info
    additional: a test for NN

--- src/main/kotlin/view/InfoFragment.kt
+++ src/main/kotlin/view/InfoFragment.kt
@@ -2,9 +2,15 @@ package view
 
 import data.SimInfo
 import data.VehicleInfo
+import javafx.embed.swing.SwingNode
 import javafx.scene.Node
+import org.knowm.xchart.*
 import presenter.SimPresenter
 import tornadofx.*
+import org.knowm.xchart.style.Styler
+import java.util.*
+import javax.swing.JComponent
+
 
 /**
  * Fragment containing various informations about the simulation current state.
@@ -12,9 +18,10 @@ import tornadofx.*
 class InfoFragment<T>(infos: T, title: String? = "", icon: Node? = null) : Fragment(title, icon) {
 
     val presenter: SimPresenter by inject()
+    val swingNode = SwingNode()
 
     override val root = vbox {
-
+        this.add(swingNode)
     }
 
     init {
@@ -34,11 +41,20 @@ class InfoFragment<T>(infos: T, title: String? = "", icon: Node? = null) : Fragm
     fun renderSimInfo(infos: SimInfo) {
         with(root) {
             this += label(infos.vehiclesCount.toString())
+            swingNode.content = vehicleSpeedDistrChart(infos)
         }
-
     }
 
-    fun vehicleSpeedDistrChart() {
+    fun vehicleSpeedDistrChart(infos: SimInfo): JComponent {
+        val chart = CategoryChartBuilder()
+            .width(800).height(600)
+            .title("Score Histogram")
+            .xAxisTitle("Score")
+            .yAxisTitle("Number")
+            .theme(Styler.ChartTheme.GGPlot2).build()
+        val hist = Histogram(infos.vehicleSpeeds.map { it.x }, 40, -10.0, 10.0)
+        chart.addSeries("vehicle speeds", hist.getxAxisData(), hist.getyAxisData())
+        return XChartPanel(chart)
     }
 
     override fun onUndock() {
